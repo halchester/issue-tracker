5281a52721282e2c55cff776664cd7c474bc5a1a
Chester Lynn <chester.htooaunglinn@gmail.com>
2021-04-02 21:33:02 +0630

    add google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,19 @@ import "tailwindcss/tailwind.css";
 import Header from "./components/Header";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "./api/query";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <Header />
         <Component {...pageProps} />
       </QueryClientProvider>
