63e98cf5c63026cef573a2f8aaf0df5297889be4
Chester Lynn <chester.htooaunglinn@gmail.com>
2021-04-19 16:31:32 +0630

    feat: turn strict mode on

--- pages/api/query.ts
+++ pages/api/query.ts
@@ -1,14 +1,15 @@
-import axios from "./index";
-import { QueryClient } from "react-query";
+import axios from './index';
+import { QueryClient } from 'react-query';
 
 export const queryClient = new QueryClient();
 
 export const fetchAllIssues = async () => {
-  const data = await axios.get("/api/issues");
-  return data.data.data;
+	const data = await axios.get('/api/issues');
+	return data.data.data;
 };
 
-export const fetchOneIssue = async (props) => {
-  const data = await axios.get(`/api/issue/${props.queryKey[1]}`);
-  return data.data.data;
+export const fetchOneIssue = async (props:any) => {
+	console.log(props);
+	const data = await axios.get(`/api/issue/${props.queryKey[1]}`);
+	return data.data.data;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,43 @@
-import Head from "next/head";
-import { useQuery } from "react-query";
-import Spinner from "../utils/Spinner";
-import { fetchAllIssues } from "./api/query";
-import IssueCard from "./components/IssueCard";
+import Head from 'next/head';
+import { useQuery } from 'react-query';
+import Spinner from '../utils/Spinner';
+import { fetchAllIssues } from './api/query';
+import IssueCard from './components/IssueCard';
+import { IssueCardProps } from './components/IssueCard';
 
 const IssueList: React.FC = () => {
-  const { isError, status, data } = useQuery(
-    "fetchAllIssues",
-    fetchAllIssues,
-    {}
-  );
-  return (
-    <div>
-      {status === "loading" ? (
-        <Spinner />
-      ) : (
-        <main className="p-4">
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {data.map((item, _) => (
-              <IssueCard
-                key={_}
-                title={item.title}
-                owner={item.owner}
-                deadline={item.deadline}
-                status={item.status}
-                type={item.type}
-                description={item.description}
-                uniqueId={item.uniqueId}
-              />
-            ))}
-          </div>
-        </main>
-      )}
-      {isError ? (
-        <p>Cannot fetch Issues! An unknown Error has occured!</p>
-      ) : null}
-    </div>
-  );
+	const { isError, status, data } = useQuery(
+		'fetchAllIssues',
+		fetchAllIssues,
+		{}
+	);
+	return (
+		<div>
+			{status === 'loading' ? (
+				<Spinner />
+			) : (
+				<main className='p-4'>
+					<div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+						{data.map((item: IssueCardProps, _: number) => (
+							<IssueCard
+								key={_}
+								title={item.title}
+								owner={item.owner}
+								deadline={item.deadline}
+								status={item.status}
+								type={item.type}
+								description={item.description}
+								uniqueId={item.uniqueId}
+							/>
+						))}
+					</div>
+				</main>
+			)}
+			{isError ? (
+				<p>Cannot fetch Issues! An unknown Error has occured!</p>
+			) : null}
+		</div>
+	);
 };
 
 export default IssueList;
